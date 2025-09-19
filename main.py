77f5442b3a79b937b29818d75ad58a2250780044
CyanCipher <heyitsmemomot@gmail.com>
2025-09-19 12:57:21 +0530

    refactor

--- main.py
+++ main.py
@@ -7,22 +7,26 @@ from habitcracker import HabitTracker
 
 def main():
     ht = HabitTracker()
-    while True:
-        usr_choice = input(
-            'Display Data(D) | Update Data(U) | Generate Graph(G) | Or EXIT: '
-        ).lower()
-        if usr_choice == 'd':
-            for line in ht.get_queries():
-                print(line)
+    try:
+        while True:
+            usr_choice = input(
+                'Display Data(D) | Update Data(U) | Generate Graph(G) | Or EXIT: '
+            ).lower()
+            if usr_choice == 'd':
+                for line in ht.get_queries():
+                    print(line)
 
-        if usr_choice == 'u':
-            ht.update()
+            if usr_choice == 'u':
+                ht.update()
 
-        if usr_choice == 'g':
-            ht.plot_data()
+            if usr_choice == 'g':
+                ht.plot_data()
 
-        if usr_choice == 'exit':
-            break
+            if usr_choice == 'exit':
+                break
+    except KeyboardInterrupt:
+        print("Keep Tracking!")
+        return
 
 
 if __name__ == "__main__":
